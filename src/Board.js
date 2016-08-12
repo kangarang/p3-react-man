ed39b85f74e5bd820f01912ccba12e812f88795f
Isaac Kang <isaacjkang@gmail.com>
2016-08-12 16:29:22 -0400

    random function

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import update from 'react-addons-update'
+
 import createTiles from './utils/CreateTiles.js';
 import TileContainer from './TileContainer.js';
 import movement from './utils/Movement.js';
@@ -11,12 +12,24 @@ class Board extends Component {
     this.state = {
       tiles: []
     }
-  }
+  };
+
+  bomb(bombIndex){
+    const App = this
+    window.setTimeout(function(){
+      App.explosion(bombIndex)},3000)
+  };
+
+  explosion(bombIndex){
+    this.setState({
+      tiles: update(this.state.tiles, {[bombIndex]: {bomb: {$set: false}}})
+    })
+  };
 
   handleKeyDown(event){
-    const position = this.state.tiles.filter(tile => tile.playerOne === true);
-    const newPosition = movement(event, position[0]);
-    const atPosition = this.state.tiles.filter(tile => tile.x === newPosition[0] && tile.y === newPosition[1] && tile.cement === false && tile.crate === false)
+    let position = this.state.tiles.filter(tile => tile.playerOne === true);
+    let newPosition = movement(event, position[0]);
+    let atPosition = this.state.tiles.filter(tile => tile.x === newPosition[0] && tile.y === newPosition[1] && tile.cement === false && tile.crate === false && tile.bomb === false)
     if (atPosition.length){
       const old = this.state.tiles.indexOf(position[0]);
       const newer = this.state.tiles.indexOf(atPosition[0]);
@@ -26,6 +39,16 @@ class Board extends Component {
       this.setState({
         tiles: update(this.state.tiles, {[newer]: {playerOne: {$set: true}}})
       })
+    } else if (newPosition === "bomb") {
+      console.log("plant bomb");
+      const i = this.state.tiles.indexOf(position[0]);
+      this.setState({
+        tiles: update(this.state.tiles, {[i]: {bomb: {$set: true}}})
+      })
+      if (this.state.tiles[i].bomb === true) {
+          return this.bomb(i);
+        };
+
     } else return
   };
 
