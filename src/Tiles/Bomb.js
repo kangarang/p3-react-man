edea5d6f909120c344af04ee5aaf2c1cc918f68e
Daniel <danielyochumjr@gmail.com>
2016-08-12 14:29:14 -0400

    added conditionals/components for new bomb tiles

---
+++ src/Tiles/Bomb.js
@@ -0,0 +1,10 @@
+import '../Board.css'
+import React from 'react';
+
+function Bomb(props){
+    return (
+      <div className="grass">BOMB</div>
+    );
+}
+
+export default Bomb;
---
+++ src/Tiles/POneBomb.js
@@ -0,0 +1,10 @@
+import '../Board.css'
+import React from 'react';
+
+function POneBomb(props){
+    return (
+      <div className="playerOne">BOMB</div>
+    );
+}
+
+export default POneBomb;
