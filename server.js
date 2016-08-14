b6f5d4c902d57ebc7cf3d302be4919ab7f387747
Isaac Kang <isaacjkang@gmail.com>
2016-08-14 17:47:57 -0400

    heroku

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 
 app.use(cors());
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/build'))
 
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 )
 
 app.listen(PORT, () =>
