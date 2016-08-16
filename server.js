4358beed24619e8399a51fbad17509e9bd368681
Isaac Kang <isaacjkang@gmail.com>
2016-08-16 10:18:25 -0400

    server

--- server.js
+++ server.js
@@ -13,6 +13,5 @@ app.get('*', (req, res) =>
 )
 
 app.listen(PORT, function() {
-  // let port = server.address().port
-  // console.log('listen to events on "port" ', port);
+  console.log(PORT);
 })
