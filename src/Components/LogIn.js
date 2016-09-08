261458ea4ee1eadace4aec7b48211e8ca9b7b088
Isaac Kang <isaacjkang@gmail.com>
2016-09-07 20:45:57 -0400

    cleaned

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -4,40 +4,40 @@ import NavLink from './NavLink';
 
 
 class LogIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email : "",
-      pw : "",
-      displayName: window.localStorage.getItem("displayName"),
-      userId: window.localStorage.getItem("uid")
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : "",
+            pw : "",
+            displayName: window.localStorage.getItem("displayName"),
+            userId: window.localStorage.getItem("uid")
+        }
     }
-  }
 
-  signIn(event){
-    event.preventDefault();
-    helpers.logIn(this.state.email,this.state.pw)
-  }
+    signIn(event){
+        event.preventDefault();
+        helpers.logIn(this.state.email,this.state.pw)
+    }
 
-  render() {
-    console.log("displayName", this.state.displayName);
-    return (
-      <div className="SignInBox">
-        <h1>Let's Log In!</h1>
-        <form className="form-inline">
-          <div className="form-group">
-            <label className="sr-only">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail3" placeholder="Email" onChange={(event) => this.setState({email: event.target.value})} />
+    render() {
+        console.log("displayName", this.state.displayName);
+        return (
+          <div className="SignInBox">
+              <h1>Let's Log In!</h1>
+              <form className="form-inline">
+                  <div className="form-group">
+                      <label className="sr-only">Email address</label>
+                      <input type="email" className="form-control" id="exampleInputEmail3" placeholder="Email" onChange={(event) => this.setState({email: event.target.value})} />
+                  </div>
+                  <div className="form-group">
+                      <label className="sr-only">Password</label>
+                      <input type="password" className="form-control" id="exampleInputPassword3" placeholder="Password" onChange={(event) => this.setState({pw: event.target.value})} />
+                  </div>
+                  <button type="submit" className="btn btn-default" onClick={(event) => this.signIn(event)}><NavLink to="/bomberman/game">Let's do this!</NavLink></button>
+              </form>
           </div>
-          <div className="form-group">
-            <label className="sr-only">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword3" placeholder="Password" onChange={(event) => this.setState({pw: event.target.value})} />
-          </div>
-          <button type="submit" className="btn btn-default" onClick={(event) => this.signIn(event)}><NavLink to="/bomberman/game">Let's do this!</NavLink></button>
-        </form>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default LogIn;
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -5,51 +5,47 @@ import NavLink from './NavLink';
 import '../Styles/App.css';
 
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: "",
-      email: "",
-      pw: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "",
+            email: "",
+            pw: ""
+        }
     }
-  }
 
-  makeUser(event){
-    event.preventDefault();
-
-    let data = {
-      userName: this.state.userName
+    makeUser(e){
+        e.preventDefault();
+        console.log(this.state.email, this.state.pw);
+        helpers.signUp(this.state.email, this.state.pw, this.state.userName)
     }
-    console.log(this.state.email,this.state.pw);
-    helpers.signUp(this.state.email,this.state.pw,data)
-  }
 
-  render() {
+    render() {
     return(
-      <div className="SignUpBox">
-        <h1>SIGN UP</h1>
-        <div>
-          <form className="form-inline">
-            <div className="form-group">
-              <label className="sr-only">Name</label>
-              <input type="text" className="form-control" id="exampleInputEmail3" placeholder="Name" onChange={(event) => this.setState({userName: event.target.value})} />
-            </div>
-            <div className="form-group">
-              <label className="sr-only">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail3" placeholder="Email" onChange={(event) => this.setState({email: event.target.value})} />
-            </div>
-            <div className="form-group">
-              <label className="sr-only">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword3" placeholder="Password" onChange={(event) => this.setState({pw: event.target.value})} />
-            </div>
+        <div className="SignUpBox">
+            <h1>SIGN UP</h1>
+            <div>
+                <form className="form-inline">
+                    <div className="form-group">
+                        <label className="sr-only">Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail3" placeholder="Name" onChange={(event) => this.setState({userName: event.target.value})} />
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail3" placeholder="Email" onChange={(event) => this.setState({email: event.target.value})} />
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword3" placeholder="Password" onChange={(event) => this.setState({pw: event.target.value})} />
+                    </div>
 
-            <button type="submit" className="btn btn-default" onClick={(event) => this.makeUser(event)}><NavLink to="/bomberman/log-in">Sign up</NavLink></button>
+                    <button type="submit" className="btn btn-default" onClick={(event) => this.makeUser(event)}><NavLink to="/bomberman/log-in">Sign up</NavLink></button>
 
-          </form>
+                </form>
+            </div>
         </div>
-      </div>
     )
-  }
+    }
 }
 
 
